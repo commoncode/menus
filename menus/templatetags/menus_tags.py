09844dc7c32422fd666d93b4c4c0775aa6356da4
Adam Hoddinott <adam@messymind.net>
2013-03-07 11:30:37 +1100

    Filter by enabled

--- menus/templatetags/menus_tags.py
+++ menus/templatetags/menus_tags.py
@@ -9,13 +9,18 @@ def render_menu(context, slug, *args, **kwargs):
 
     try:
         if getattr(Menu.objects, "platform", False):
-            query = Menu.objects.platform(request.platform).get(slug=slug)
+            query = Menu.objects.platform(request.platform).get(
+                enabled=True,
+                slug=slug
+                )
         else:
-            query = Menu.objects.get(slug=slug)
+            query = Menu.objects.get(
+                enabled=True,
+                slug=slug
+                )
     except Menu.DoesNotExist, Menu.MultipleObjectsReturned:
         query = None
 
-    # import ipdb; ipdb.set_trace()
     context["menu"] = query
     context["current_path"] = request.path
     return context
