e5ccdfe589af23c2f78b89bd51210021629ab60e
Y3K <dboy.y3k@gmail.com>
2014-04-11 15:58:06 -0500

    Fakers

--- menus/factories.py
+++ menus/factories.py
@@ -1,11 +1,11 @@
 import factory
 
 from django.contrib.contenttypes.models import ContentType
-from django.contrib.webdesign import lorem_ipsum
 from django.template.defaultfilters import slugify
 
 from faker import Factory
 
+from .fakers import words
 
 
 fake = Factory.create()
@@ -14,10 +14,8 @@ fake = Factory.create()
 class LinkFactory(factory.django.DjangoModelFactory):
     FACTORY_FOR = 'menus.Link'
 
-    title = factory.LazyAttribute(
-        lambda o: lorem_ipsum.words(2, common=False).title())
-    slug = factory.LazyAttribute(
-        lambda o: slugify(lorem_ipsum.words(3, common=False)))
+    title = factory.LazyAttribute(lambda o: words(2).title())
+    slug = factory.LazyAttribute(lambda o: slugify(words(3)))
     content_type = ContentType.objects.get(app_label='products',
         model='category')
 
@@ -25,10 +23,8 @@ class LinkFactory(factory.django.DjangoModelFactory):
 class MenuFactory(factory.django.DjangoModelFactory):
     FACTORY_FOR = 'menus.Menu'
 
-    name = factory.LazyAttribute(
-        lambda o: lorem_ipsum.words(2, common=False).title())
-    slug = factory.LazyAttribute(
-        lambda o: slugify(lorem_ipsum.words(3, common=False).title()))
+    name = factory.LazyAttribute(lambda o: words(2).title())
+    slug = factory.LazyAttribute(lambda o: slugify(words(3)))
 
 
 class MenuItemFactory(factory.django.DjangoModelFactory):
