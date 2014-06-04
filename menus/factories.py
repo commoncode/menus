68107b1bb3a88e5cb375e7da05430aeb76f4df98
Y3K <dboy.y3k@gmail.com>
2014-06-04 14:54:03 -0500

    Factories Fix

--- menus/factories.py
+++ menus/factories.py
@@ -12,6 +12,7 @@ fake = Factory.create()
 
 class LinkFactory(factory.django.DjangoModelFactory):
     FACTORY_FOR = 'menus.Link'
+    FACTORY_DJANGO_GET_OR_CREATE = ('title', )
 
     title = factory.LazyAttribute(lambda o: words(2).title())
     slug = factory.LazyAttribute(lambda o: slugify(words(3)))
